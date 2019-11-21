1679f650938868983f670d8f1be9e2bdc1a07dba
Steven Swanson <swanson@cs.ucsd.edu>
2019-11-21 04:50:11 +0000

    fine tuning

--- bin/local_dev.sh
+++ bin/local_dev.sh
@@ -2,9 +2,11 @@ V=0.10
 
 docker run -it \
        --volume $HOME:$HOME \
+       --volume /tmp:/tmp \
        --volume /var/run/docker.sock:/var/run/docker.sock \
+       --privileged \
        -e HOME=$HOME \
        -e PS1='${HOSTNAME}:$PWD ' \
        --hostname $(hostname)-DOCKER \
-       -w $HOME\
-       stevenjswanson/cse141pp:latest
+       -w $HOME \
+       $DOCKER_IMAGE
--- cloud/runner.sh
+++ cloud/runner.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-docker run \
+docker run -it \
        --volume /var/run/docker.sock:/var/run/docker.sock \
-       --volume /tmp/:/tmp/ \
-       -e SUBMISSION_DIR='/submission' \
+       --privileged \
        --hostname $(hostname)-RUNNER \
-       -w /course/$CONFIG_REPO_ROOT_NAME \
        $DOCKER_IMAGE \
-       bash -c 'DEPLOYMENT_MODE=TESTING; . config.sh;  runall.d'
-
-
+       bash -c 'cd $CONFIG_REPO_ROOT_NAME; export DEPLOYMENT_MODE=TESTING; . config.sh; runlab.d'
 
